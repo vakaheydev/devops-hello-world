fdffbe4c7af573148d6ca9afb7e710b37fa76521
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2025-01-31 17:56:53 +0300

    feature/add-output-parameters added str parameters to output

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
+        String argsStr = Arrays.toString(args);
+        System.out.println("Args: " + argsStr);
         System.out.println("Hello world Changed! " + now);
     }
 }
